b3f93e88878586d7f76389dceb25c947008ea6b5
unknown <w17390@W17390.wasion.cn>
2015-09-28 11:22:48 +0800

    assignments:ASSN 0

---
+++ my_answer/ps0.py
@@ -0,0 +1,10 @@
+# Problem Set 0
+# Name: xin zhong
+# Collaborators: 
+# Time: 8:58-9:03 
+#
+last_name = input("Enter your last name:\n**")
+first_name = input("Enter your first name:\n**")
+print(first_name)
+print(last_name)
+
